6e7a0842a588d995b80be3a55bfa386b2f3c7033
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-03-28 11:02:30 +0200

    Started working on Skills part.

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -1,8 +1,49 @@
 import React from "react";
 import styles from "../../styles/Skills.module.css";
+import Image from "next/image";
+import C from "../../images/c-.png";
+import HTML from "../../images/html5.png";
+import CSS from "../../images/css3.png";
+import JS from "../../images/js.png";
+import Next from "../../images/next-dot-js.png";
+import Node from "../../images/node-js.png";
+import ReactPNG from "../../images/react.png";
+import SQL from "../../images/sql-server.png";
 
 const Skills = () => {
-  return <div className="container">Skills</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner_container}>
+        <h2 className={styles.title}>SKILLS</h2>
+        <div className={styles.grid_container}>
+          <div className={styles.grid_item}>
+            <Image src={C} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={HTML} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={CSS} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={JS} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={Next} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={Node} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={ReactPNG} layout="fixed" />
+          </div>
+          <div className={styles.grid_item}>
+            <Image src={SQL} layout="fixed" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
