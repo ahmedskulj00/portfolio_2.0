7778bd2b1bbe3b9b583334bea02288289fc06d97
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-04 13:32:56 +0200

    Complete overhaul of the project.
    I decided to completely change the design of the porfolio, therefore I deleted everything I did, because I didn't want to create a new repository. The reason I didn't create a new repository is because I want to track everything I did with this project.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import About from "../components/about/About";
-import Navbar from "../components/navbar/Navbar";
-import Skills from "../components/skills/Skills";
-import Work from "../components/work/Work";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <About />
-      {/* <Skills /> */}
-      <Work />
-    </div>
-  );
+  return <div></div>;
 }
