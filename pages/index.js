b594a396119d0e146634748be090d0b442e17699
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-06 16:26:46 +0200

    Updating title and icon.

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,15 @@ import Experience from "../components/Experience";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Ahmed Skulj</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Hero />
       <div className={styles.dottedpart_container}>
