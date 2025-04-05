c352e29e40be7f648e3cda1278dca14a5eae476d
Ahmed Skulj <skuljahmed00@gmail.com>
2025-04-05 22:30:41 +0200

    Removing contact section

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import About from "../components/About";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
-import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Head from "next/head";
 
@@ -32,7 +31,6 @@ export default function Home() {
         <DottedPart />
       </div>
       <Projects />
-      <Contact />
       <Footer />
     </div>
   );
